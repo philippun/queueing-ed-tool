cb6df252509d2ef8e3c038306e2d95ae09af7f2f
philippun <philippungrund@icloud.com>
2021-06-07 15:56:18 +0200

    extended general structure to include the basic layout of the navbarpage. Create about mardown file

---
+++ src/about.md
@@ -0,0 +1,2 @@
+### This is the about page
+It will give some context about this project and tool eventually.
--- src/app.R
+++ src/app.R
@@ -1,7 +1,50 @@
 library(shiny)
 
-ui <- fluidPage(
-  
+settings <- function() {
+    tabPanel(
+        "Settings",
+        h3("Patients"),
+        sliderInput("arrivalRate",
+                    "Arrival rate:",
+                    min = 1,
+                    max = 10,
+                    value = 5),
+        h3("Hospitals"),
+        checkboxInput("pooled",
+                      "Pool queues",
+                      value = FALSE),
+        sliderInput("serviceRate",
+                    "Service rate:",
+                    min = 1,
+                    max = 10,
+                    value = 5)
+    )
+}
+
+scenarios <- function() {
+    tabPanel(
+        "Scenarios"
+    )
+}
+
+app <- function() {
+    sidebarLayout(
+        sidebarPanel(
+            tabsetPanel(
+                settings(),
+                scenarios()
+            ),
+            width = 3
+        ),
+        mainPanel()
+    )
+}
+
+ui <- navbarPage(
+    "Queueing Education Tool",
+    selected = "App",
+    tabPanel("App", app()),
+    tabPanel("About", includeMarkdown("about.md"))
 )
 
 server <- function(input, output, session) {
